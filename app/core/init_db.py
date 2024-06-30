d9faa29388b5e6401cfda705fdcb639389576ec4
Daniil V <ntqcby@inbox.ru>
2024-06-30 05:24:12 +0300

    pep

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -14,10 +14,9 @@ get_user_db_context = contextlib.asynccontextmanager(get_user_db)
 get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
 
 
-async def create_user(
-        email: EmailStr,
-        password: str,
-        is_superuser: bool = False):
+async def create_user(email: EmailStr,
+                      password: str,
+                      is_superuser: bool = False):
     try:
         async with get_async_session_context() as session:
             async with get_user_db_context(session) as user_db:
@@ -35,10 +34,11 @@ async def create_user(
 
 async def create_first_superuser():
     if (
-        settings.first_superuser_email is not None and
-        settings.first_superuser_password is not None
-        ):
+        settings.first_superuser_email is not None
+        and settings.first_superuser_password is not None
+    ):
         await create_user(
             email=settings.first_superuser_email,
             password=settings.first_superuser_password,
-            is_superuser=True,)
+            is_superuser=True,
+        )
