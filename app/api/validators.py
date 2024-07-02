c8836305e46f9ca15aa4996acc8915caca0aa542
Daniil V <ntqcby@inbox.ru>
2024-07-02 23:09:24 +0300

    peps+docstr

--- app/api/validators.py
+++ app/api/validators.py
@@ -11,6 +11,7 @@ async def check_charity_project_name_duplicate(
     project_name: str,
     session: AsyncSession,
 ):
+    """Проверяет, существует ли проект с таким же именем."""
     project_id = await charity_project_crud.get_charity_project_by_name(
         project_name, session
     )
@@ -23,6 +24,7 @@ async def check_charity_project_name_duplicate(
 
 def check_charity_project_invested_amount(
         project: CharityProject, new_amount: int):
+    """Проверяет, нельзя ли установить сумму, ниже уже вложенной."""
     if project.invested_amount > new_amount:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
@@ -34,6 +36,7 @@ async def check_charity_project_exists(
     project_id: int,
     session: AsyncSession,
 ):
+    """Проверяет, существует ли проект с заданным идентификатором."""
     project = await charity_project_crud.get_charity_project_by_id(
         project_id, session)
     if project is None:
@@ -44,6 +47,7 @@ async def check_charity_project_exists(
 
 
 def check_charity_project_not_empty(charity_project: CharityProject):
+    """Проверяет, нет ли в проекте вложений."""
     if charity_project.invested_amount > 0:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
@@ -52,6 +56,7 @@ def check_charity_project_not_empty(charity_project: CharityProject):
 
 
 def check_charity_project_is_closed(charity_project: CharityProject):
+    """Проверяет, закрыт ли проект."""
     if charity_project.fully_invested:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
